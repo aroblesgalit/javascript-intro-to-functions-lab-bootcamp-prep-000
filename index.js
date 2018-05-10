021901454a125390506c23151c0f1ed9203633f4
Alvin Galit <aroblesgalit@gmail.com>
2018-05-10 04:54:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string===string.toLowerCase()) {
     return "I can't hear you!";
-  } else if(string===)
-
-function sayHiToGrandma(string) {
-  console.log(string.toUpperCase());
-  return "YES INDEED!";
-}
+  } else if(string===string.toUpperCase()) {
+    return "YES INDEED!";
+  } else {
+    return "I love you, too.";
+  }
